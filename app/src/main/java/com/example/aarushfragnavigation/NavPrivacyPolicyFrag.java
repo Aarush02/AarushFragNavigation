20f02fce51d92066da52a978401309cab8dccff9
Aarush02 <43503017+Aarush02@users.noreply.github.com>
2020-07-13 10:33:28 +0530

    navigation fragment added

---
+++ app/src/main/java/com/example/aarushfragnavigation/NavPrivacyPolicyFrag.java
@@ -0,0 +1,20 @@
+package com.example.aarushfragnavigation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.zip.Inflater;
+
+public class NavPrivacyPolicyFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.nav_privacy_policy_frag, null);
+    }
+}
